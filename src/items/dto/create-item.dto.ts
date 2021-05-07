99584dee414afd5d7e60f2984b22db2691628f2f
Muritala David <davmuri1414@gmail.com>
2021-05-07 23:19:54 +0100

    added validation

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, IsNotEmpty, Min } from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+
+  @Min(5)
   readonly qty: number;
 }
